6c944453491fd36fc138bcfe1b9cae1032955d2b
BigHoch <143054658+BigHoch@users.noreply.github.com>
2023-09-13 17:22:44 -0400

    Finalized documents
    All comments have been added.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a complete to do list utilizing HTML, CSS, and Javascript. Comments detailing the values effected are located in the line above each respecitve code block.
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Creates a 'close button' utilizing the document.createTextNode("\u00d7") it then creates a 'span' element and assigns "close" as its class.//
+// myNodelist is then looped through and the span variable is appeneded to each element.//
 var myNodelist = document.getElementsByTagName("Li");
 var i; 
 for (i = 0; i <myNodelist.length; i++) {
@@ -8,7 +10,8 @@ for (i = 0; i <myNodelist.length; i++) {
     myNodelist[i].appendChild(span);
 }
 
-
+// This block creates an array called 'close' and assignes all elements of the same class name to it.//
+// When an element with the "close" class is clicked, it hides its parent element by setting its style.display property to "none.
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -17,14 +20,15 @@ for (i = 0; i < close.length; i++) {
         div.style.display = "none";
     }
 }
-
+// This line selects the first <ul> element found in the HTML document using the querySelector method. It assigns this element to the list variable.
+// list.addEventListener('click', function(ev) { ... }, false);: This line adds a click event listener to the list (the <ul> element).
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
 }, false);
-
+// Creates a new list item when clicking on the add button, if the input value is nothing, 'You must write something' will appear above.
 function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
